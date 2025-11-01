ddff6d97fbca5f269730734e7ca1b22941b4b45e
Demon437 <namdevshanu206@gmail.com>
2025-11-01 15:30:07 +0530

    coded

---
+++ brigh-stageback/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ContestantViewSet
+from .views import *
+router = DefaultRouter()
+router.register(r'contestants', ContestantViewSet, basename='contestant')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/google/', google_login, name='google_login'),
+path('user/google/', GoogleLoginView.as_view(), name='google-login'),
+path("api/admin/login/", LoginAPIView.as_view(), name="fixed-login"),
+]
+
+
